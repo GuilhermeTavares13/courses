b9b3573373b1113acdcbffacd276025cadc9ed0a
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2024-10-21 23:31:26 -0300

    Update Course

--- Full Stack Web Developer/Drum Kit Starting Files/index.js
+++ Full Stack Web Developer/Drum Kit Starting Files/index.js
@@ -1,7 +1,45 @@
+document.addEventListener("keypress", (event) => {
+    retriveInstrumentAudio(event.key);
+});
+
+
+
 for (let i = 0; i <= document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-        let audio = new Audio('./sounds/'+document.querySelectorAll(".drum").style.backgroundImage+'.mp3');
-        audio.play();
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+       retriveInstrumentAudio(this.innerHTML);
     });
 }
 
+function retriveInstrumentAudio(instrument) {
+
+    let instrumentSelected;
+    switch (instrument) {
+        case 'w':
+            instrumentSelected = 'tom-1';
+            break;
+        case 'a':
+            instrumentSelected = 'tom-2';
+            break;
+        case 's':
+            instrumentSelected = 'tom-3';
+            break;
+        case 'd':
+            instrumentSelected = 'tom-4';
+            break;
+        case 'j':
+            instrumentSelected = 'snare';
+            break;
+        case 'k':
+            instrumentSelected = 'crash';
+            break;
+        case 'l':
+            instrumentSelected = 'kick-bass';
+            break;
+        default:
+            console.log(content);
+            break;
+    }
+    let audio = new Audio('./sounds/'+instrumentSelected+'.mp3');
+    audio.play();
+}
