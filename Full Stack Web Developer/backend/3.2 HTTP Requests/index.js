6030623e0ff2f399a68c552ac9960d10dc5a2b9d
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2024-10-28 21:28:20 -0300

    Update Course

---
+++ Full Stack Web Developer/backend/3.2 HTTP Requests/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Test!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1><p>My name is Guilherme</p>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Me</h1><p>Phone: +1899918999</p>");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
